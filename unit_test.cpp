2735beff079881c83f1cddf6038ea2606dc24530
darrien gunn <dgunn001@ucr.edu>
2019-01-31 14:54:46 -0800

    edit test

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,11 +1,38 @@
-#include "c-echo.h"
+#include "base.h"
+#include "base.cpp"
 
 #include "gtest/gtest.h"
 
-TEST(EchoTest, ExampleUnitTest){
-   char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "29";
-   EXPECT_EQ("29", echo(2,test_val));
+
+TEST(AddTest, AddDouble){
+   Op* num1 = new Op(1.1);
+   Op* num2 = new Op(2.2);
+   Add* result = new Add(num1,num2);
+   EXPECT_EQ(3.3,result->evaluate());
 }
+
+TEST(MulTest, MulDouble){
+   Op* num1 = new Op(1.1);
+   Op* num2 = new Op(2.2);
+   Mul* result = new Mul(num1,num2);
+   EXPECT_EQ(2.42, result->evaluate());
+}
+
+TEST(SubTest, SubDouble){
+   Op* num1 = new Op(1.1);
+   Op* num2 = new Op(2.2);
+   Sub* result = new Sub(num1,num2);
+   EXPECT_EQ(-1.1, result->evaluate());
+}
+
+TEST(DivTest, DivDouble){
+   Op* num1 = new Op(1.1);
+   Op* num2 = new Op(2.2);
+   Div* result = new Div(1.1,2.2);
+   EXPECT_EQ(0.5, result->evaluate());
+}
+
+
 
 int main(int argc, char **argv){
    ::testing::InitGoogleTest(&argc, argv);
