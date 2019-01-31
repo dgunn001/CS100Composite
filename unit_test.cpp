86b70f0ab4577d273be55895bb3c6e86fdd83a7b
darrien gunn <dgunn001@ucr.edu>
2019-01-31 15:13:18 -0800

    unit testing

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,38 +1,60 @@
 #include "base.h"
 #include "base.cpp"
+#include "SevenOpMock.h"
+#include "FiveOpMock.h"
 
 #include "gtest/gtest.h"
 
+TEST(OpTest, OpSevenMock){
+   Op* num1 = new SevenOpMock();
+   Op* num2 = new Op(7.5);
+   EXPECT_EQ(num1,num2);
+}
+
+TEST(RandTest, RandMock){
+   Op* num1 = new RandMock();
+   Op* num2 = new Op(000);
+   EXPECT_EQ(num1,num2);
+}
 
 TEST(AddTest, AddDouble){
-   Op* num1 = new Op(1.1);
-   Op* num2 = new Op(2.2);
+   Op* num1 = new SevenOpMock();
+   Op* num2 = new FiveOpMock();
    Add* result = new Add(num1,num2);
-   EXPECT_EQ(3.3,result->evaluate());
+   EXPECT_EQ(2.5,result->evaluate());
 }
 
 TEST(MulTest, MulDouble){
-   Op* num1 = new Op(1.1);
-   Op* num2 = new Op(2.2);
+   Op* num1 = new SevenOpMock();
+   Op* num2 = new FiveOpMock();
    Mul* result = new Mul(num1,num2);
-   EXPECT_EQ(2.42, result->evaluate());
+   EXPECT_EQ(37.5, result->evaluate());
 }
 
 TEST(SubTest, SubDouble){
-   Op* num1 = new Op(1.1);
-   Op* num2 = new Op(2.2);
+   Op* num1 = new SevenOpMock();
+   Op* num2 = new FiveOpMock();
    Sub* result = new Sub(num1,num2);
-   EXPECT_EQ(-1.1, result->evaluate());
+   EXPECT_EQ(2.5, result->evaluate());
 }
 
 TEST(DivTest, DivDouble){
-   Op* num1 = new Op(1.1);
-   Op* num2 = new Op(2.2);
-   Div* result = new Div(1.1,2.2);
-   EXPECT_EQ(0.5, result->evaluate());
+   Op* num1 = new Op(7.5);
+   Op* num2 = new Op(5);
+   Div* result = new Div(num1,num2);
+   EXPECT_EQ(1.5, result->evaluate());
 }
 
+TEST(ComboTest, DivAddSub){
+   Op* num1 = new Op(7.5);
+   Op* num2 = new Op(5);
+   Op* num3 = new Op(3);
+   Mul* result = new Mul(num1,num2);
+   Add* result2 = new Add(result, num3);
+   EXPECT_EQ(40.5,result->evaluate());
+}
 
+ 
 
 int main(int argc, char **argv){
    ::testing::InitGoogleTest(&argc, argv);
