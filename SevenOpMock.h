99918467abea1ccfc2ea4cdf9cd5527970313f49
Darrien Gunn <38056323+dgunn001@users.noreply.github.com>
2019-01-31 16:14:26 -0800

    Delete SevenOpMock.h

--- SevenOpMock.h
+++
@@ -1,9 +0,0 @@
-#include "base.h"
-
-class SevenOpMock: public Base {
-   public:
-      OpMock(){ };
-
-      virtual double evaluate(){ return 7.5;}
-      virtual string stringify(){ return "7.5";}
-}
